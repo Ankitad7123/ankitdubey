1ff659791f327f49d970dc4bf064d039d0df2abb
ankitad7123 <ankit11akhil@gmail.com>
2024-09-20 12:08:46 +0530

    skip 10

--- src/components/Counting.jsx
+++ src/components/Counting.jsx
@@ -429,7 +429,7 @@ const Counting = ({ onComplete }) => {
     if (window.innerWidth < 600) {
       return {
         position: 'absolute',
-        top: '-48px',
+        top: '-45px',
         right: '10px',
 
         color:"green",
